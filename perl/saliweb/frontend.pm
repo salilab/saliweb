156b727ffcdfa270976bab34d060923363e24570
Ben Webb <ben@salilab.org>
2009-09-01 20:30:36 +0000

    Add a function to choose a unique job name.

--- perl/saliweb/frontend.pm
+++ perl/saliweb/frontend.pm
@@ -1,7 +1,7 @@
 package saliweb::frontend;
 require Exporter;
 @ISA = qw(Exporter);
-@EXPORT = qw(read_config connect_to_database);
+@EXPORT = qw(read_config connect_to_database make_job);
 
 use File::Spec;
 use DBI;
@@ -45,6 +45,49 @@ sub connect_to_database {
                          $config->{database}->{passwd})
             or die "Cannot connect to database: $!";
   return $dbh;
+}
+
+sub try_job_name {
+  my ($jobname, $query, $dbh, $config) = @_;
+  my $jobdir = $config->{directories}->{INCOMING} . "/" . $jobname;
+  if (-d $jobdir) {
+    return;
+  }
+  $query->execute($jobname) or die "Cannot execute: " . $dbh->errstr;
+  my @data = $query->fetchrow_array();
+  if ($data[0] == 0) {
+    mkdir($jobdir) or die "Cannot make job directory $jobdir: $!";
+    $query->execute($jobname) or die "Cannot execute: " . $dbh->errstr;
+    @data = $query->fetchrow_array();
+    if ($data[0] == 0) {
+      return $jobdir;
+    }
+  }
+}
+
+sub make_job {
+  my ($config, $dbh, $user_jobname) = @_;
+  # Remove potentially dodgy characters in jobname
+  $user_jobname =~ s/[^a-zA-Z0-9_-]//g;
+  # Make sure jobname fits in the db (plus extra space for
+  # auto-generated suffix if needed)
+  $user_jobname = substr($user_jobname, 0, 30);
+
+  my $query = $dbh->prepare('select count(name) from jobs where name=?')
+                 or die "Cannot prepare query ". $dbh->errstr;
+  my ($jobname, $jobdir);
+  $jobdir = try_job_name($user_jobname, $query, $dbh, $config);
+  if ($jobdir) {
+    return ($user_jobname, $jobdir);
+  }
+  for (my $tries = 0; $tries < 50; $tries++) {
+    $jobname = $user_jobname . "_" . int(rand(100000)) . $tries;
+    $jobdir = try_job_name($jobname, $query, $dbh, $config);
+    if ($jobdir) {
+      return ($jobname, $jobdir);
+    }
+  }
+  die "Could not determine a unique job name";
 }
 
 1;
