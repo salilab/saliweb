c7deb065bc51661501289b1f695aa0c21260266d
Ben Webb <ben@salilab.org>
2009-09-08 04:49:19 +0000

    Add a verbose option; by default, swallow any statefile exception.

--- python/saliweb/backend/process_jobs.py
+++ python/saliweb/backend/process_jobs.py
@@ -1,3 +1,27 @@
+from optparse import OptionParser
+import saliweb.backend
+import sys
+
+def get_options():
+    parser = OptionParser()
+    parser.set_usage("""
+%prog [-h] [-v]
+
+Do any necessary processing of incoming, completed, or old jobs.
+""")
+    parser.add_option('-v', '--verbose', action="store_true", dest="verbose",
+                      help="""Print verbose output""")
+    opts, args = parser.parse_args()
+    if len(args) != 0:
+        parser.error("Extra arguments given")
+    return opts
+
 def main(webservice):
+    opts = get_options()
     web = webservice.get_web_service(webservice.config)
-    web.do_all_processing()
+    try:
+        web.do_all_processing()
+    except saliweb.backend.StateFileError, detail:
+        if opts.verbose:
+            raise
+        # else ignore the exception
