aa8a0de4881775bbe5b984b1b0d6db329856a19b
Ben Webb <ben@salilab.org>
2009-10-07 19:41:46 +0000

    Have make_job return an IncomingJob object that presents a simpler interface.

--- perl/saliweb/frontend.pm
+++ perl/saliweb/frontend.pm
@@ -1,3 +1,118 @@
+package IncomingJob;
+
+sub new {
+    my ($invocant, $frontend, $given_name, $email) = @_;
+    my $class = ref($invocant) || $invocant;
+    my $self = {};
+    bless($self, $class);
+    $self->{frontend} = $frontend;
+    $self->{given_name} = $given_name;
+    $self->{email} = $email;
+    ($self->{name}, $self->{directory}) = _get_job_name_directory($frontend,
+                                                                  $given_name);
+    ($self->{url}, $self->{passwd}) = _generate_results_url($frontend,
+                                                            $self->{name});
+    return $self;
+}
+
+sub name {
+    my $self = shift;
+    return $self->{name};
+}
+
+sub directory {
+    my $self = shift;
+    return $self->{directory};
+}
+
+sub results_url {
+    my $self = shift;
+    return $self->{url};
+}
+
+sub submit {
+  my $self = shift;
+  my $config = $self->{frontend}->{'config'};
+  my $dbh = $self->{frontend}->{'dbh'};
+
+  # Insert row into database table
+  my $query = "insert into jobs (name,passwd,contact_email,directory,url," .
+              "submit_time) VALUES(?, ?, ?, ?, ?, UTC_TIMESTAMP())";
+  my $in = $dbh->prepare($query) or die "Cannot prepare query ". $dbh->errstr;
+  $in->execute($self->{name}, $self->{passwd}, $self->{email},
+               $self->{directory}, $self->{url})
+        or die "Cannot execute query " . $dbh->errstr;
+
+  # Use socket to inform backend of new incoming job
+  my $s = IO::Socket::UNIX->new(Peer=>$config->{general}->{'socket'},
+                                Type=>SOCK_STREAM);
+  if (defined($s)) {
+    print $s "INCOMING " . $self->{name};
+    $s->close();
+  }
+}
+
+sub _get_job_name_directory {
+  my ($frontend, $user_jobname) = @_;
+  my $config = $frontend->{'config'};
+  my $dbh = $frontend->{'dbh'};
+  # Remove potentially dodgy characters in jobname
+  $user_jobname =~ s/[^a-zA-Z0-9_-]//g;
+  # Make sure jobname fits in the db (plus extra space for
+  # auto-generated suffix if needed)
+  $user_jobname = substr($user_jobname, 0, 30);
+
+  my $query = $dbh->prepare('select count(name) from jobs where name=?')
+                 or die "Cannot prepare query ". $dbh->errstr;
+  my ($jobname, $jobdir);
+  $jobdir = try_job_name($user_jobname, $query, $dbh, $config);
+  if ($jobdir) {
+    return ($user_jobname, $jobdir);
+  }
+  for (my $tries = 0; $tries < 50; $tries++) {
+    $jobname = $user_jobname . "_" . int(rand(100000)) . $tries;
+    $jobdir = try_job_name($jobname, $query, $dbh, $config);
+    if ($jobdir) {
+      return ($jobname, $jobdir);
+    }
+  }
+  die "Could not determine a unique job name";
+}
+
+sub _generate_results_url {
+    my ($frontend, $jobname) = @_;
+    my $passwd = &generate_random_passwd(10);
+    $url = $frontend->cgiroot . "/results.cgi?job=$jobname&passwd=$passwd";
+    return ($url, $passwd);
+}
+
+sub generate_random_passwd {
+  # Generate a random alphanumeric password of the given length
+  my ($len) = @_;
+  my @validchars = ('a'..'z', 'A'..'Z', 0..9);
+  my $randstr = join '', map $validchars[rand @validchars], 1..$len;
+  return $randstr;
+}
+
+sub try_job_name {
+  my ($jobname, $query, $dbh, $config) = @_;
+  my $jobdir = $config->{directories}->{INCOMING} . "/" . $jobname;
+  if (-d $jobdir) {
+    return;
+  }
+  $query->execute($jobname) or die "Cannot execute: " . $dbh->errstr;
+  my @data = $query->fetchrow_array();
+  if ($data[0] == 0) {
+    mkdir($jobdir) or die "Cannot make job directory $jobdir: $!";
+    $query->execute($jobname) or die "Cannot execute: " . $dbh->errstr;
+    @data = $query->fetchrow_array();
+    if ($data[0] == 0) {
+      return $jobdir;
+    }
+  }
+}
+
+
 package saliweb::frontend;
 
 use saliweb::server qw(validate_user);
@@ -359,68 +474,9 @@ sub help_link {
 }
 
 sub make_job {
-  my ($self, $user_jobname) = @_;
-  my $config = $self->{'config'};
-  my $dbh = $self->{'dbh'};
-  # Remove potentially dodgy characters in jobname
-  $user_jobname =~ s/[^a-zA-Z0-9_-]//g;
-  # Make sure jobname fits in the db (plus extra space for
-  # auto-generated suffix if needed)
-  $user_jobname = substr($user_jobname, 0, 30);
-
-  my $query = $dbh->prepare('select count(name) from jobs where name=?')
-                 or die "Cannot prepare query ". $dbh->errstr;
-  my ($jobname, $jobdir);
-  $jobdir = try_job_name($user_jobname, $query, $dbh, $config);
-  if ($jobdir) {
-    return ($user_jobname, $jobdir);
-  }
-  for (my $tries = 0; $tries < 50; $tries++) {
-    $jobname = $user_jobname . "_" . int(rand(100000)) . $tries;
-    $jobdir = try_job_name($jobname, $query, $dbh, $config);
-    if ($jobdir) {
-      return ($jobname, $jobdir);
-    }
-  }
-  die "Could not determine a unique job name";
+  my ($self, $user_jobname, $email) = @_;
+  return new IncomingJob($self, $user_jobname, $email);
 }
-
-sub generate_results_url {
-    my ($self, $jobname) = @_;
-    my $passwd = &generate_random_passwd(10);
-    $url = $self->cgiroot . "/results.cgi?job=$jobname&passwd=$passwd";
-    return ($url, $passwd);
-}
-
-sub submit_job {
-  my ($self, $jobname, $passwd, $email, $jobdir, $url) = @_;
-  my $config = $self->{'config'};
-  my $dbh = $self->{'dbh'};
-
-  # Insert row into database table
-  my $query = "insert into jobs (name,passwd,contact_email,directory,url," .
-              "submit_time) VALUES(?, ?, ?, ?, ?, UTC_TIMESTAMP())";
-  my $in = $dbh->prepare($query) or die "Cannot prepare query ". $dbh->errstr;
-  $in->execute($jobname, $passwd, $email, $jobdir, $url)
-        or die "Cannot execute query " . $dbh->errstr;
-
-  # Use socket to inform backend of new incoming job
-  my $s = IO::Socket::UNIX->new(Peer=>$config->{general}->{'socket'},
-                                Type=>SOCK_STREAM);
-  if (defined($s)) {
-    print $s "INCOMING $jobname";
-    $s->close();
-  }
-}
-
-sub generate_random_passwd {
-  # Generate a random alphanumeric password of the given length
-  my ($len) = @_;
-  my @validchars = ('a'..'z', 'A'..'Z', 0..9);
-  my $randstr = join '', map $validchars[rand @validchars], 1..$len;
-  return $randstr;
-}
-
 
 sub read_ini_file {
   my ($filename) = @_;
@@ -462,23 +518,3 @@ sub connect_to_database {
             or die "Cannot connect to database: $!";
   return $dbh;
 }
-
-sub try_job_name {
-  my ($jobname, $query, $dbh, $config) = @_;
-  my $jobdir = $config->{directories}->{INCOMING} . "/" . $jobname;
-  if (-d $jobdir) {
-    return;
-  }
-  $query->execute($jobname) or die "Cannot execute: " . $dbh->errstr;
-  my @data = $query->fetchrow_array();
-  if ($data[0] == 0) {
-    mkdir($jobdir) or die "Cannot make job directory $jobdir: $!";
-    $query->execute($jobname) or die "Cannot execute: " . $dbh->errstr;
-    @data = $query->fetchrow_array();
-    if ($data[0] == 0) {
-      return $jobdir;
-    }
-  }
-}
-
-1;
