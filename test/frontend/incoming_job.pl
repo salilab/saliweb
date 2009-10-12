d0f4d331905fa543bb5c059523fae280b9046a69
Ben Webb <ben@salilab.org>
2009-10-12 06:00:49 +0000

    Test generate_random_passwd function.

--- test/frontend/incoming_job.pl
+++ test/frontend/incoming_job.pl
@@ -11,5 +11,17 @@ BEGIN { use_ok('saliweb::frontend'); }
 # Test sanitize_jobname function
 {
     is(saliweb::frontend::IncomingJob::_sanitize_jobname("ABC&^abc; 123_-X"),
-       "ABCabc123_-X", "sanitize_jobname");
+       "ABCabc123_-X", "sanitize_jobname (chars)");
+
+    is(saliweb::frontend::IncomingJob::_sanitize_jobname(
+         "012345678901234567890123456789abcdefghijlk"),
+         "012345678901234567890123456789", "                 (length)");
+}
+
+# Test generate_random_passwd function
+{
+    my $str = saliweb::frontend::IncomingJob::generate_random_passwd(10);
+    is(length($str), 10, "generate_random_password (10)");
+    $str = saliweb::frontend::IncomingJob::generate_random_passwd(20);
+    is(length($str), 20, "                         (20)");
 }
