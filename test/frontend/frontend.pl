0031cc759afeeabfc9d1e989cce6fba68815f905
Ben Webb <ben@salilab.org>
2009-10-12 20:02:37 +0000

    Check more frontend methods and free functions.

--- test/frontend/frontend.pl
+++ test/frontend/frontend.pl
@@ -24,3 +24,106 @@ BEGIN { use_ok('saliweb::frontend'); }
               '<img class="helplink" src="/saliweb/img/help.jpg" ' .
               'alt="help" /></a>' . "\n", "check help_link");
 }
+
+# Test _admin_email method
+{
+    my $default = 'system@salilab.org';
+    my $self = {};
+    bless($self, 'saliweb::frontend');
+    is($self->_admin_email, $default, 'admin_email (default 1)');
+    $self->{config} = {};
+    is($self->_admin_email, $default, '            (default 2)');
+    $self->{config}->{general} = {};
+    is($self->_admin_email, $default, '            (default 3)');
+    $self->{config}->{general}->{admin_email} = "myemail";
+    is($self->_admin_email, "myemail", '            (config)');
+}
+
+# Test set_page_title method
+{
+    my $self = {server_name=>'myserver'};
+    bless($self, 'saliweb::frontend');
+    $self->set_page_title("test page");
+    is($self->{page_title}, "myserver test page", 'set_page_title');
+}
+
+# Test start_html method
+{
+    my $self = {CGI=>new CGI, page_title=>'test page title'};
+    bless($self, 'saliweb::frontend');
+    like($self->start_html(),
+         '/<!DOCTYPE html.*<head>.*<title>test page title<\/title>' .
+         '.*<link rel="stylesheet" type="text\/css" ' .
+         'href="\/saliweb\/css\/server.css" \/>' .
+         '.*<script src="\/saliweb\/js\/salilab\.js".*<\/head>' .
+         '.*<body onload=/s', "start_html (default style)");
+    like($self->start_html('mystyle.css'),
+         '/<!DOCTYPE html.*<head>.*<title>test page title<\/title>' .
+         '.*<link rel="stylesheet".*href="mystyle.css"' .
+         '.*<script src="\/saliweb\/js\/salilab\.js".*<\/head>' .
+         '.*<body onload=/s', "           (mystyle)");
+}
+
+# Test end_html method
+{
+    my $self = {CGI=>new CGI};
+    bless($self, 'saliweb::frontend');
+    like($self->end_html(), qr/<\/body>.*<\/html>/s, "end_html");
+}
+
+# Test get_projects method
+{
+    my $self = {CGI=>new CGI};
+    bless($self, 'saliweb::frontend');
+    my $proj = $self->get_projects();
+    is(%$proj, 0, 'get_projects returns an empty hashref');
+}
+
+# Test get_project_menu method
+{
+    my $self = {CGI=>new CGI};
+    bless($self, 'saliweb::frontend');
+    is($self->get_project_menu, "", 'get_project_menu returns an empty string');
+}
+
+# Test get_navigation_links method
+{
+    my $self = {CGI=>new CGI};
+    bless($self, 'saliweb::frontend');
+    my $links = $self->get_navigation_links();
+    is(@$links, 0, 'get_navigation_links returns an empty arrayref');
+}
+
+# Test check_required_email method
+{
+    throws_ok { check_required_email(undef) }
+              saliweb::frontend::InputValidationError,
+              "check_required_email (undef)";
+
+    throws_ok { check_required_email("") }
+              saliweb::frontend::InputValidationError,
+              "                     (\"\")";
+
+    throws_ok { check_required_email("garbage") }
+              saliweb::frontend::InputValidationError,
+              "                     (invalid address)";
+
+    lives_ok { check_required_email("test\@test.com") }
+              "                     (good address)";
+}
+
+# Test check_optional_email method
+{
+    lives_ok { check_optional_email(undef) }
+             "check_optional_email (undef)";
+
+    lives_ok { check_optional_email("") }
+             "                     (\"\")";
+
+    throws_ok { check_optional_email("garbage") }
+              saliweb::frontend::InputValidationError,
+              "                     (invalid address)";
+
+    lives_ok { check_optional_email("test\@test.com") }
+              "                     (good address)";
+}
