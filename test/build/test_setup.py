5d3422f31510c75c7c30153e016ad723a1758b7b
Ben Webb <ben@salilab.org>
2009-10-15 17:33:32 +0000

    Check _setup_version function.

--- test/build/test_setup.py
+++ test/build/test_setup.py
@@ -1,5 +1,27 @@
 import unittest
 import saliweb.build
+import warnings
+import tempfile
+import shutil
+import os
+import re
+
+def run_catch_warnings(method, *args, **keys):
+    """Run a method and return both its own return value and a list of any
+       warnings raised."""
+    warnings.simplefilter("always")
+    oldwarn = warnings.showwarning
+    w  = []
+    def myshowwarning(*args):
+        w.append(args)
+    warnings.showwarning = myshowwarning
+
+    try:
+        ret = method(*args, **keys)
+        return ret, w
+    finally:
+        warnings.showwarning = oldwarn
+        warnings.resetwarnings()
 
 class DummyConfig: pass
 
@@ -37,6 +59,70 @@ class SetupTest(unittest.TestCase):
         del env['config']
         # Make sure it didn't create any additional directory keys
         self.assertEqual(env, {})
+
+    def test_setup_version(self):
+        """Check _setup_version function"""
+        class BrokenEnv(dict):
+            def __init__(self, bin):
+                dict.__init__(self)
+                self.bin = bin
+            def WhereIs(self, bin): return self.bin
+        tmpdir = tempfile.mkdtemp()
+        def get_broken_env_pyscript(name, script):
+            tmpfile = os.path.join(tmpdir, name)
+            print >> open(tmpfile, 'w'), "#!/usr/bin/python\n" + script
+            os.chmod(tmpfile, 0755)
+            return BrokenEnv(tmpfile)
+
+        # Check with provided version number
+        env = {}
+        saliweb.build._setup_version(env, '1.0')
+        self.assertEqual(env, {'version': '1.0'})
+
+        # No number provided; no svnversion binary in path
+        env = BrokenEnv(None)
+        ret, warns = run_catch_warnings(saliweb.build._setup_version, env, None)
+        self.assertEqual(len(warns), 1)
+        self.assertEqual(warns[0][0].args,
+                         ("Could not find 'svnversion' binary in path",))
+        self.assertEqual(env, {'version': None})
+
+        # No number provided; cannot find svnversion binary
+        env = BrokenEnv('/not/exist/svnversion')
+        ret, warns = run_catch_warnings(saliweb.build._setup_version, env, None)
+        self.assertEqual(len(warns), 1)
+        self.assertEqual(warns[0][0].args,
+                         ("Could not run /not/exist/svnversion: [Errno 2] "
+                          "No such file or directory",))
+        self.assertEqual(env, {'version': None})
+
+        # No number provided; svnversion binary reports 'exported'
+        env = get_broken_env_pyscript('expscript', 'print "exported"')
+        ret, warns = run_catch_warnings(saliweb.build._setup_version, env, None)
+        self.assertEqual(len(warns), 0)
+        self.assertEqual(env, {'version': None})
+
+        # No number provided; svnversion binary returns error
+        env = get_broken_env_pyscript('errscript', """
+import sys
+print >> sys.stderr, "error text"
+print "output text"
+sys.exit(1)""")
+        ret, warns = run_catch_warnings(saliweb.build._setup_version, env, None)
+        self.assertEqual(len(warns), 1)
+        self.assert_(re.match('Could not run \S+\/errscript: returned exit '
+                              'code 1, stdout output text\n, '
+                              'stderr error text\n$', warns[0][0].args[0]),
+                     "%s does not match re" % warns[0][0].args[0])
+        self.assertEqual(env, {'version': None})
+
+        # No number provided; svnversion binary works
+        env = get_broken_env_pyscript('workscript', 'print "1024\\n2048"')
+        ret, warns = run_catch_warnings(saliweb.build._setup_version, env, None)
+        self.assertEqual(len(warns), 0)
+        self.assertEqual(env, {'version': 'r1024'}) # Only first line used
+
+        shutil.rmtree(tmpdir, ignore_errors=True)
 
 if __name__ == '__main__':
     unittest.main()
