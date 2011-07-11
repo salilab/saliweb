2783c073062ca46dd252d17823ffba6811c4ed84
Ben Webb <ben@salilab.org>
2011-07-11 19:12:34 +0000

    Test resubmit script.

---
+++ test/backend/test_resubmit.py
@@ -0,0 +1,59 @@
+import unittest
+import sys
+from saliweb.backend.resubmit import main
+import StringIO
+
+class ResubmitTest(unittest.TestCase):
+    """Check resubmit script"""
+
+    def test_main(self):
+        """Test resubmit main()"""
+        class DummyJob(object):
+            def __init__(self, mod):
+                self.mod = mod
+            def resubmit(self):
+                self.mod.job_resub = True
+        class DummyWebService(object):
+            def __init__(self, mod):
+                self.mod = mod
+            def get_job_by_name(self, state, name):
+                if state == 'FAILED' and name == 'testjob':
+                    return DummyJob(self.mod)
+                else:
+                    return None
+        class DummyModule(object):
+            config = 'testconfig'
+            job_resub = False
+            def get_web_service(self, config):
+                return DummyWebService(self)
+
+        old = sys.argv
+        olderr = sys.stderr
+        try:
+            sys.stderr = StringIO.StringIO()
+            mod = DummyModule()
+            sys.argv = ['testprogram']
+            self.assertRaises(SystemExit, main, mod)
+
+            sio = StringIO.StringIO()
+            sys.stderr = sio
+            mod = DummyModule()
+            sys.argv = ['testprogram'] + ['badjob']
+            main(mod)
+            self.assertEqual(sio.getvalue(), 'Could not find job badjob\n')
+            self.assertEqual(mod.job_resub, False)
+
+            sio = StringIO.StringIO()
+            sys.stderr = sio
+            mod = DummyModule()
+            sys.argv = ['testprogram'] + ['testjob']
+            main(mod)
+            self.assertEqual(sio.getvalue(), '')
+            self.assertEqual(mod.job_resub, True)
+        finally:
+            sys.argv = old
+            sys.stderr = olderr
+
+
+if __name__ == '__main__':
+    unittest.main()
