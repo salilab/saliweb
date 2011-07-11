bc1475d7a664f75660c8590b457de1d361c99f1c
Ben Webb <ben@salilab.org>
2011-07-11 19:03:47 +0000

    Test deljob script.

--- test/backend/test_deljob.py
+++ test/backend/test_deljob.py
@@ -1,6 +1,9 @@
 import unittest
+import sys
 from saliweb.backend import InvalidStateError
-from saliweb.backend.deljob import check_valid_state
+from saliweb.backend.deljob import check_valid_state, get_options, delete_job
+from saliweb.backend.deljob import main
+import StringIO
 
 class DummyWeb(object):
     def __init__(self, pid):
@@ -25,6 +28,112 @@ class DelJobTest(unittest.TestCase):
             check_valid_state(web, state)
         for state in ('COMPLETED', 'RUNNING'):
             self.assertRaises(ValueError, check_valid_state, web, state)
+
+    def test_get_options(self):
+        """Test deljob get_options()"""
+        def run_get_options(args):
+            old = sys.argv
+            oldstderr = sys.stderr
+            try:
+                sys.stderr = StringIO.StringIO()
+                sys.argv = ['testprogram'] + args
+                return get_options()
+            finally:
+                sys.stderr = oldstderr
+                sys.argv = old
+        self.assertRaises(SystemExit, run_get_options, [])
+        state, jobname, opts = run_get_options(['FAILED', 'testjob'])
+        self.assertEqual(state, 'FAILED')
+        self.assertEqual(jobname, 'testjob')
+        self.assertEqual(opts.force, False)
+        for arg in ['-f', '--force']:
+            state, jobname, opts = run_get_options([arg, 'FAILED', 'testjob'])
+            self.assertEqual(opts.force, True)
+
+    def test_delete_job(self):
+        """Test deljob delete_job()"""
+        class DummyJob(object):
+            deleted = False
+            name = 'testjob'
+            def delete(self):
+                self.deleted = True
+        class DummyStdin(object):
+            def __init__(self, answer):
+                self.answer = answer
+            def readline(self):
+                return self.answer
+        def run_delete_job(job, force, answer):
+            oldout = sys.stdout
+            oldin = sys.stdin
+            try:
+                sio = StringIO.StringIO()
+                sys.stdout = sio
+                sys.stdin = DummyStdin(answer)
+                delete_job(job, force)
+                return sio.getvalue()
+            finally:
+                sys.stdout = oldout
+                sys.stdin = oldin
+        j = DummyJob()
+        out = run_delete_job(j, True, None)
+        self.assertEqual(j.deleted, True)
+        self.assertEqual(out, '')
+        for no in ('', 'n', 'x'):
+            j = DummyJob()
+            out = run_delete_job(j, False, no)
+            self.assertEqual(j.deleted, False)
+            self.assertEqual(out, 'Delete job testjob? ')
+        for yes in ('y', 'Y', 'yes'):
+            j = DummyJob()
+            out = run_delete_job(j, False, yes)
+            self.assertEqual(j.deleted, True)
+            self.assertEqual(out, 'Delete job testjob? ')
+
+    def test_main(self):
+        """Test deljob main()"""
+        class DummyJob(object):
+            def __init__(self, mod):
+                self.mod = mod
+            def delete(self):
+                self.mod.job_deleted = True
+        class DummyWebService(object):
+            def __init__(self, mod):
+                self.mod = mod
+            def get_running_pid(self):
+                return 9999
+            def get_job_by_name(self, state, name):
+                if state == 'FAILED' and name == 'testjob':
+                    return DummyJob(self.mod)
+                else:
+                    return None
+        class DummyModule(object):
+            config = 'testconfig'
+            job_deleted = False
+            def get_web_service(self, config):
+                return DummyWebService(self)
+
+        old = sys.argv
+        olderr = sys.stderr
+        try:
+            sio = StringIO.StringIO()
+            sys.stderr = sio
+            mod = DummyModule()
+            sys.argv = ['testprogram'] + ['-f', 'FAILED', 'badjob']
+            main(mod)
+            self.assertEqual(sio.getvalue(), 'Could not find job badjob\n')
+            self.assertEqual(mod.job_deleted, False)
+
+            sio = StringIO.StringIO()
+            sys.stderr = sio
+            mod = DummyModule()
+            sys.argv = ['testprogram'] + ['-f', 'FAILED', 'testjob']
+            main(mod)
+            self.assertEqual(sio.getvalue(), '')
+            self.assertEqual(mod.job_deleted, True)
+        finally:
+            sys.argv = old
+            sys.stderr = olderr
+
 
 if __name__ == '__main__':
     unittest.main()
