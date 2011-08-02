de196a36575d4c3f85017827a6c919fb711e019e
Ben Webb <ben@salilab.org>
2011-08-02 16:16:07 +0000

    Allow resubmitting multiple jobs.

--- python/saliweb/backend/resubmit.py
+++ python/saliweb/backend/resubmit.py
@@ -5,21 +5,22 @@ import sys
 def get_options():
     parser = OptionParser()
     parser.set_usage("""
-%prog [-h] JOBNAME
+%prog [-h] JOBNAME [...]
 
-Take the given failed job, JOBNAME, and put it back in the incoming queue.
+Take the given failed job(s), JOBNAME, and put them back in the incoming queue.
 """)
     opts, args = parser.parse_args()
-    if len(args) != 1:
+    if len(args) < 1:
         parser.error("Need to specify a job name")
-    return args[0]
+    return args
 
 
 def main(webservice):
-    name = get_options()
+    names = get_options()
     web = webservice.get_web_service(webservice.config)
-    job = web.get_job_by_name('FAILED', name)
-    if job:
-        job.resubmit()
-    else:
-        print >> sys.stderr, "Could not find job", name
+    for name in names:
+        job = web.get_job_by_name('FAILED', name)
+        if job:
+            job.resubmit()
+        else:
+            print >> sys.stderr, "Could not find job", name
