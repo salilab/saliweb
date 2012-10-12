638646f388cfd63b98a56da1e749810461c42139
Ben Webb <ben@salilab.org>
2012-10-12 20:12:44 +0000

    Add license text.

--- python/saliweb/web_service.py
+++ python/saliweb/web_service.py
@@ -9,6 +9,22 @@
    command line interface to the services.
 """
 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# Copyright (c) 2012 Sali Lab
+
 import sys
 import os
 from xml.dom.minidom import parseString
