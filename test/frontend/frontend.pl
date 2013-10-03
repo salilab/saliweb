25f4f974b4e4233504529010f3d5129b1c64d117
Ben Webb <ben@salilab.org>
2013-10-03 18:25:06 +0000

    Test the new _google_ua method.

--- test/frontend/frontend.pl
+++ test/frontend/frontend.pl
@@ -46,6 +46,20 @@ BEGIN {
          'alt="help" \/></a>\s*$/s', "check help_link");
 }
 
+# Test _google_ua method
+{
+    my $default = 'UA-44577804-1';
+    my $self = {};
+    bless($self, 'saliweb::frontend');
+    is($self->_google_ua, $default, 'google_ua (default 1)');
+    $self->{config} = {};
+    is($self->_google_ua, $default, '          (default 2)');
+    $self->{config}->{general} = {};
+    is($self->_google_ua, $default, '          (default 3)');
+    $self->{config}->{general}->{google_ua} = "test_ua";
+    is($self->_google_ua, "test_ua", '          (config)');
+}
+
 # Test _admin_email method
 {
     my $default = 'system@salilab.org';
