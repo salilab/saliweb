f42141aac1bf4024b78567cbce0140828b06f696
Ben Webb <ben@salilab.org>
2014-08-04 21:31:25 +0000

    Add AllosMod to nightly builds.

--- tools/auto-build.sh
+++ tools/auto-build.sh
@@ -19,7 +19,8 @@ rm -rf ${TMPDIR}
 mkdir ${TMPDIR}
 cd ${TMPDIR}
 
-for REPO in saliweb modloop multifit_web evaluation ligscore modweb salign; do
+for REPO in saliweb modloop multifit_web evaluation ligscore modweb
+            salign allosmod; do
   SVNDIR=file:///cowbell1/svn/${REPO}/trunk/
   SWSRCTGZ=${MODINSTALL}/build/sources/private/${REPO}.tar.gz
 
