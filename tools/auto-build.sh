c776e64d361aa00e5fcb8e1c6c1d6c3ab95a40db
Ben Webb <ben@salilab.org>
2014-08-05 15:05:58 +0000

    Fix syntax.

--- tools/auto-build.sh
+++ tools/auto-build.sh
@@ -19,7 +19,7 @@ rm -rf ${TMPDIR}
 mkdir ${TMPDIR}
 cd ${TMPDIR}
 
-for REPO in saliweb modloop multifit_web evaluation ligscore modweb
+for REPO in saliweb modloop multifit_web evaluation ligscore modweb \
             salign allosmod; do
   SVNDIR=file:///cowbell1/svn/${REPO}/trunk/
   SWSRCTGZ=${MODINSTALL}/build/sources/private/${REPO}.tar.gz
