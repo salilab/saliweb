8c0f1741f015c8ffd475a2639a113abcec8e6bba
Ben Webb <ben@salilab.org>
2015-09-03 12:00:36 -0700

    Update copyright year.

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sali lab web framework'
-copyright = u'2009-2014, Sali Lab'
+copyright = u'2009-2015, Sali Lab'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
