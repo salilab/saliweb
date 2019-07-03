652c95022080ca682cfa2523f1d3f9bf37e8827d
Ben Webb <ben@salilab.org>
2019-07-02 18:46:37 -0700

    Add a simple function to make a Flask app
    Add a function with creates a Flask app, sets up
    email-on-error logging, and reads a web service
    config file to populate the Flask config.

--- python/saliweb/frontend/__init__.py
+++ python/saliweb/frontend/__init__.py
@@ -1,5 +1,8 @@
-from flask import Blueprint, url_for, Markup
+from flask import Blueprint, url_for, Markup, Flask
 import datetime
+import ConfigParser
+import os
+import logging.handlers
 
 
 def _format_timediff(timediff):
@@ -56,3 +59,50 @@ class CompletedJob(object):
 
 
 blueprint = Blueprint('saliweb', __name__, template_folder='templates')
+
+
+def _read_config(app, fname):
+    """Read the webservice configuration file and set Flask-style app.config
+       from it. Flask config names are all uppercase and have the config
+       section (except for [general]) prefixed."""
+    config = ConfigParser.SafeConfigParser()
+
+    with open(fname) as fh:
+        config.readfp(fh, fname)
+
+    for section in config.sections():
+        prefix = '' if section == 'general' else section.upper() + '_'
+        for name, value in config.items(section):
+            app.config[prefix + name.upper()] = value
+
+    config_dir = os.path.dirname(os.path.abspath(fname))
+    frontend_db = config.get('database', 'frontend_config')
+    fname = os.path.join(config_dir, frontend_db)
+    with open(fname) as fh:
+        config.readfp(fh, fname)
+    for name in ('user', 'passwd'):
+        value = config.get('frontend_db', name)
+        app.config["DATABASE_" + name.upper()] = value
+
+
+def _setup_email_logging(app):
+    if not app.debug:
+        mail_handler = logging.handlers.SMTPHandler(
+            mailhost=('localhost', 25),
+            fromaddr='no-reply@modbase.compbio.ucsf.edu',
+            toaddrs=[app.config['ADMIN_EMAIL']],
+            subject='%s web service error' % app.config['SERVICE_NAME'])
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+
+def make_application(name, config, *args, **kwargs):
+    """Make and return a Flask application.
+       `name` should normally be `__name__`; `config` is the path to the
+       web service config file. Other arguments are passed to the Flask
+       constructor."""
+    app = Flask(name, *args, **kwargs)
+    _read_config(app, config)
+    _setup_email_logging(app)
+    app.register_blueprint(blueprint)
+    return app
