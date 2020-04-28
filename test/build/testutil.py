690d15bc735836511bf7ff92f010920ea7708f9d
Ben Webb <ben@salilab.org>
2020-04-27 19:10:15 -0700

    Provide assertIsNot for Python 2.6

--- test/build/testutil.py
+++ test/build/testutil.py
@@ -19,6 +19,8 @@ if not hasattr(unittest.TestCase, 'assertIsInstance'):
                         % (member, container))
     def assertIs(self, a, b, msg=None):
         return self.assertTrue(a is b, msg or '%s is not %s' % (a, b))
+    def assertIsNot(self, a, b, msg=None):
+        return self.assertTrue(a is not b, msg or '%s is %s' % (a, b))
     def assertIsInstance(self, obj, cls, msg=None):
         return self.assertTrue(isinstance(obj, cls),
                         msg or '%s is not an instance of %s' % (obj, cls))
@@ -59,6 +61,7 @@ if not hasattr(unittest.TestCase, 'assertIsInstance'):
     unittest.TestCase.assertIn = assertIn
     unittest.TestCase.assertNotIn = assertNotIn
     unittest.TestCase.assertIs = assertIs
+    unittest.TestCase.assertIsNot = assertIsNot
     unittest.TestCase.assertIsInstance = assertIsInstance
     unittest.TestCase.assertLess = assertLess
     unittest.TestCase.assertGreater = assertGreater
