d0d25632cb037c57fd6db737b58f9f7653690213
Ben Webb <ben@salilab.org>
2025-09-29 12:01:23 -0700

    Add missing file

---
+++ python/saliweb/config.py
@@ -0,0 +1,14 @@
+# Location of all SVN repositories
+svn_url_top = 'https://svn.example.com/'
+
+# GitHub organization containing all git repositories
+github_org = 'salilab'
+
+# URL for the wiki page listing UIDs of all web service users
+wiki_uid_page = 'https://example.com/uid'
+
+# Directory under which all web service user accounts are created
+home_top = '/home'
+
+# Email domain for admin contacts
+email_domain = 'example.com'
